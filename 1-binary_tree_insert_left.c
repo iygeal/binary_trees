c092b87af85e634f6f61481cd1948bc3b2252ce5
Innocent Anozie <iygeal@gmail.com>
2024-04-10 10:03:02 +0100

    Refactor

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -22,11 +22,9 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->right = NULL;
 
 	if (parent->left != NULL)
-	{
 		parent->left->parent = new_node;
-	}
-	else if (parent->left == NULL)
-		parent->left = new_node;
+
+	parent->left = new_node;
 
 	return (new_node);
 }
