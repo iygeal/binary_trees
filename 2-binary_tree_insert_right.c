45556be8be8c97880ec08a42f2a77c4ab6327894
Innocent Anoziee <iygeal@gmail.com>
2024-04-10 14:08:40 +0100

    Refactored to assign new node to parent right first before checking if it's NULL

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -9,11 +9,11 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	if (parent == NULL)
+	new_node = malloc(sizeof(binary_tree_t));
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
@@ -21,11 +21,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 
 	new_node->right = parent->right;
 	new_node->left = NULL;
+	parent->right = new_node;
 
 	if (parent->right != NULL)
 		parent->right->parent = new_node;
-
-	parent->right = new_node;
 
 	return (new_node);
 
