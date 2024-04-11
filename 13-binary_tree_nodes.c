fbe835c24849e733bc79115fbe6edd1840dca875
Innocent Anoziee <iygeal@gmail.com>
2024-04-11 15:40:32 +0100

    Refactored code to count all nodes with at least one child correctly

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -11,11 +11,14 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree == NULL)
 		return (0);
 
-	if (tree->left != NULL || tree->right != NULL)
-		return (1);
-
+	/* If subtree has any child, count the nodes recursively; else, count 0 */
 	left_child = tree->left ? binary_tree_nodes(tree->left) : 0;
 	right_child = tree->right ? binary_tree_nodes(tree->right) : 0;
 
+	/* If current node has child/ren, count 1 + sum of children*/
+	if (tree->left != NULL || tree->right != NULL)
+		return (1 + (left_child + right_child));
+
+	/* If current node has no child, return sum of children*/
 	return (left_child + right_child);
 }
